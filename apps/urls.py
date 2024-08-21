1f8d1b32d2218e8b77bba39ba5a83b6524e582ca
SunnatDevPy <147712581+SunnatDevPy@users.noreply.github.com>
2024-08-21 15:42:32 +0500

    Update urls.py

--- apps/urls.py
+++ apps/urls.py
@@ -16,6 +16,5 @@ urlpatterns = [
 
     path('token', LoginView.as_view(), name='token_obtain_pair'),
     path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
-    path('register', RegisterView.as_view(), name='register')
 
 ]
